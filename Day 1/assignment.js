16d6fdd8057866edd7ad6915504eb760e3285604
Manan <135083037+NeupaneManan@users.noreply.github.com>
2023-12-08 20:22:54 +0545

    Add files via upload

---
+++ Day 1/assignment.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 2;
+
+let c = a + b;
+console.log(`c = a + b: ${c}`);
+
+c += 3;
+console.log(`c += 3: ${c}`);
+
+c -= 2;
+console.log(`c -= 2: ${c}`);
+
+c *= 4;
+console.log(`c *= 4: ${c}`);
+
+c /= 2;
+console.log(`c /= 2: ${c}`);
---
+++ Day 1/comparison.js
@@ -0,0 +1,9 @@
+let a=30
+let b=45
+
+if(a>b){
+    console.log(a +"is greater than"+ b);
+}
+else{
+    console.log( b+ "is greater")
+}
